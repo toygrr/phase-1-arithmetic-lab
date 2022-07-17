17cbbaa7f60c5347a7e82577836056089208aada
Tyler <toygrrr@gmail.com>
2022-07-17 13:39:44 -0700

    commit tree 18799d6ea1ae33eb910c130f8a1b37b5e57bd8a2

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
 
-// Write your code here
+const multiply = num1 * num2;
+const random = Math.round(Math.random() + 1)
+
+const num3 = 19;
+const num4 = 5;
+
+const mod = num3 % num4;
+
+const max = Math.max(5, 6 ,18, 20);
